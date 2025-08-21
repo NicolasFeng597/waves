1e1e414369943e2c839132e8a33b30e65d96ff2f
Nicolas Feng <nicolasfeng597@gmail.com>
2025-08-21 16:41:43 -0400

    deleting redunant files

--- Live_Input.py
+++ Live_Input.py
@@ -1,11 +1,12 @@
 """
-Live input module. 
+Live input module.
 
 This module contains the LiveInput class for live audio input.
 """
 
 import pyaudio
 import numpy as np
+
 
 class LiveInput:
     """
@@ -18,11 +19,16 @@ class LiveInput:
     def __init__(self):
         self.audio = pyaudio.PyAudio()
         info = self.audio.get_host_api_info_by_index(0)
-        numdevices = info.get('deviceCount')
-        
+        numdevices = info.get("deviceCount")
+
         print("\n---------------------- record device list ----------------------")
         for i in range(numdevices):
-            if self.audio.get_device_info_by_host_api_device_index(0, i).get("maxInputChannels") > 0:
+            if (
+                self.audio.get_device_info_by_host_api_device_index(0, i).get(
+                    "maxInputChannels"
+                )
+                > 0
+            ):
                 print(
                     f"Input Device {i}"
                     + f" - {self.audio.get_device_info_by_host_api_device_index(0, i).get("name")}"
